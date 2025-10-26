a4eac097865332185bc162761e86aad0ca553f2a
Adnan Ali <adnan.ali@gmail.com>
2025-10-25 21:49:50 -0400

    Refactor gem so that letter models can be provided by external gems.
    letter_opener_web-redis, for example.

--- lib/letter_opener_web.rb
+++ lib/letter_opener_web.rb
@@ -2,11 +2,16 @@
 
 require 'letter_opener_web/version'
 require 'letter_opener_web/engine'
+require 'letter_opener_web/letter_interface'
 require 'rexml/document'
 
 module LetterOpenerWeb
   class Config
-    attr_accessor :letters_location
+    attr_accessor :letters_location, :letter_class
+
+    def initialize
+      @letter_class = LetterOpenerWeb::Letter
+    end
   end
 
   def self.config
